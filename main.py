9289cda4a07e82a4a0c072d26c7c32f38740bb9e
barelik35 <barelik35@gmail.com>
2023-03-11 09:04:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.n11.com/bilgisayar/dizustu-bilgisayar"
+
+html = requests.get(url).content
+soup = BeautifulSoup(html, "html.parser")
+
+list = soup.find_all("li", {"class":"column"},limit=1)
+print(list)
